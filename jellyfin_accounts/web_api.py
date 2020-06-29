52a11c3905794e77e770b1abe0a1afa62294c87c
Harvey Tindall <harveyltindall@gmail.com>
2020-06-29 23:24:54 +0100

    auth fix

--- jellyfin_accounts/web_api.py
+++ jellyfin_accounts/web_api.py
@@ -358,7 +358,7 @@ def modifyConfig():
 
 
 @app.route("/getConfig", methods=["GET"])
-# @auth.login_required
+@auth.login_required
 def getConfig():
     log.debug('Config requested')
     with open(config_base_path, "r") as f:
